0fb1d9ae4dd2cef55ee820756e6b619497d4026c
Hosan_Lee <hosahn13@gmail.com>
2022-10-19 14:58:19 +0200

    Fisrt Commit

---
+++ src/main.java
@@ -0,0 +1,15 @@
+import java.io.IOException;
+import java.util.Scanner;
+
+public class main {
+    // main seed
+    public static void main(String[] args) throws IOException {
+        Scanner sc = new Scanner(System.in);
+        int x = sc.nextInt();
+        int y = sc.nextInt();
+    }
+    // functions
+    static int result(int width, int length){
+        return width * length;
+    }
+}
